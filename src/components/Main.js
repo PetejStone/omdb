f8339c235e3edb064fd738467c8dcb10ec172762
PetejStone <petejstoney@gmail.com>
2020-12-29 15:18:58 -0800

    disabled buttons in local storage if added

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,22 +4,35 @@ import './Main.css'
 import {useEffect, useState} from 'react'
 const Main = props => {
    const [data,setData] = useState('')
-   const [nominations,setNominations] = useState([])
-   
-   let [count, setCount] = useState(0)
+   let retrievedData = localStorage.getItem("noms");
+   let parsed = JSON.parse(retrievedData);
+   let retrievedKeys = localStorage.getItem("keys");
+   let parsedKeys = JSON.parse(retrievedKeys);
+   const [nominations,setNominations] = useState( parsed || [] )
+   const [keys, setKeys] = useState(parsedKeys || {})
+
    
    useEffect(()=>{
         setData(props.data.Search)
-        //setNominations(nominations)
+        localStorage.setItem('noms',JSON.stringify(nominations))
+        localStorage.setItem('keys', JSON.stringify(keys))
+        setNominations(nominations)
+        setKeys(keys)
         // setCount(count)
-    },[nominations, props.data])
+        
+    },[nominations, props.data,keys])
     
-    const handleClick = (movie) => {
+    const handleClick = (movie,index) => {
+        console.log(movie)
       const newNoms = [...nominations]
       newNoms.push(movie)
       setNominations(newNoms)
-      console.log(nominations)
+      setKeys({
+        [index]: true
+      })
+      //localStorage.setItem('noms',nominations)
     }
+
 
     return (
         <div className="main">
@@ -29,7 +42,8 @@ const Main = props => {
                     data && data.map((movie,index) => 
                     <div className="movie" key={index}>
                         <h4 className="title">Title: {movie.Title} ({movie.Year})</h4>
-                        <button className="nominate" onClick={() => handleClick(movie)}>Nominate</button>
+                         {console.log('keys',keys)}
+                         <button id={index} className="nominate" disabled={keys[index] } onClick={() => handleClick(movie,index)}>Nominate</button>
                     </div>
                     )
                 }
@@ -37,13 +51,12 @@ const Main = props => {
 
             <div className="nominations">
                 <h2>Nominations:</h2>
-                {console.log(nominations)}
 
                 {
                     nominations && nominations.map((movie,index) => 
                     <div className="movie" key={index}>
                         <h4 className="title">Title: {movie.Title} ({movie.Year})</h4>
-                        <button className="nominate" onClick={() => handleClick(movie)}>Nominate</button>
+                        <button className="nominate" onClick={() => handleClick(movie)}>Remove</button>
                     </div>
                     )
                 }
