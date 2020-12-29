d9ec5e2eec4887a4ac3aef04d4192ee7a8d1d334
PetejStone <petejstoney@gmail.com>
2020-12-29 13:45:36 -0800

    added nominations section

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,20 +4,46 @@ import './Main.css'
 import {useEffect, useState} from 'react'
 const Main = props => {
    const [data,setData] = useState('')
-    
-    useEffect(()=>{
+   const [nominations,setNominations] = useState([])
+   
+   let [count, setCount] = useState(0)
+   
+   useEffect(()=>{
         setData(props.data.Search)
-    },[props.data])
+       /// setNominations(nominations)
+        // setCount(count)
+    },[props.data,nominations])
+    
+    const handleClick = (movie) => {
+      setNominations(...[nominations],nominations.push(movie))
+    }
+
     return (
         <div className="main">
             <div className="results">
+                <h2>Results:</h2>
                 {
-                    data && data.map((movie,index) => <div className="movie" key={index}>Title:{movie.Title} ({movie.Year})</div>)
+                    data && data.map((movie,index) => 
+                    <div className="movie" key={index}>
+                        <h4 className="title">Title: {movie.Title} ({movie.Year})</h4>
+                        <button className="nominate" onClick={() => handleClick(movie)}>Nominate</button>
+                    </div>
+                    )
                 }
             </div>
 
             <div className="nominations">
-                
+                <h2>Nominations:</h2>
+
+                {
+                    nominations && nominations.map((movie,index) => 
+                    <div className="movie" key={index}>
+                        <h4 className="title">Title: {movie.Title} ({movie.Year})</h4>
+                        <button className="nominate" onClick={() => handleClick(movie)}>Nominate</button>
+                    </div>
+                    )
+                }
+               
             </div>
             
         </div>
